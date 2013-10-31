c12113c16c09140abccfdfb4c8cd46b183e07243
Alvaro <alvieboy@alvie.com>
2013-10-31 10:48:42 +0000

    Add build for binutils

--- build-binutils.sh
+++ build-binutils.sh
@@ -2,13 +2,16 @@
 
 if [ -d binutils ]; then
 	echo "'binutils' folder already exists, remove it first"
-        exit -1;
+        exit 255
 fi
 
 mkdir -p binutils
 mkdir -p install
-cd binutils
+(
+cd binutils;
 
 ../binutils-2.23.1/configure \
-	--target xtc-elf 
+	--target xtc-elf \
         --prefix=$(pwd)/../install/
+)
+echo "Now, type 'make' inside binutils folder"
