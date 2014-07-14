d69f340b64dece7d4dc99640873d9012eeda402f
Alvaro <alvieboy@alvie.com>
2014-07-14 17:08:25 +0100

    Fix testbench

--- tb.vhd
+++ tb.vhd
@@ -17,159 +17,6 @@ architecture sim of tb is
   signal w_clk_2x: std_logic := '1';
   signal w_rst: std_logic := '0';
 
-  signal wb_read:    std_logic_vector(31 downto 0);
-  signal wb_write:   std_logic_vector(31 downto 0);
-  signal wb_address: std_logic_vector(31 downto 0);
-  signal wb_tag_i:   std_logic_vector(31 downto 0);
-  signal wb_tag_o:   std_logic_vector(31 downto 0);
-  signal wb_stb:     std_logic;
-  signal wb_cyc:     std_logic;
-  signal wb_sel:     std_logic_vector(3 downto 0);
-  signal wb_we:      std_logic;
-  signal wb_ack:     std_logic;
-  signal wb_stall:     std_logic;
-  signal wb_int: std_logic := '0';
-
-  signal rom_wb_ack:       std_logic;
-  signal rom_wb_read:      std_logic_vector(31 downto 0);
-  signal rom_wb_adr:       std_logic_vector(31 downto 0);
-  signal rom_wb_cyc:       std_logic;
-  signal rom_wb_stb:       std_logic;
-  signal rom_wb_cti:       std_logic_vector(2 downto 0);
-  signal rom_wb_stall:     std_logic;
-
-  component wbarb2_1 is
-  generic (
-    ADDRESS_HIGH: integer := 31;
-    ADDRESS_LOW: integer := 0
-  );
-  port (
-    wb_clk_i: in std_logic;
-	 	wb_rst_i: in std_logic;
-
-    -- Master 0 signals
-
-    m0_wb_dat_o: out std_logic_vector(31 downto 0);
-    m0_wb_dat_i: in std_logic_vector(31 downto 0);
-    m0_wb_adr_i: in std_logic_vector(ADDRESS_HIGH downto ADDRESS_LOW);
-    m0_wb_sel_i: in std_logic_vector(3 downto 0);
-    m0_wb_cti_i: in std_logic_vector(2 downto 0);
-    m0_wb_we_i:  in std_logic;
-    m0_wb_cyc_i: in std_logic;
-    m0_wb_stb_i: in std_logic;
-    m0_wb_stall_o: out std_logic;
-    m0_wb_ack_o: out std_logic;
-
-    -- Master 1 signals
-
-    m1_wb_dat_o: out std_logic_vector(31 downto 0);
-    m1_wb_dat_i: in std_logic_vector(31 downto 0);
-    m1_wb_adr_i: in std_logic_vector(ADDRESS_HIGH downto ADDRESS_LOW);
-    m1_wb_sel_i: in std_logic_vector(3 downto 0);
-    m1_wb_cti_i: in std_logic_vector(2 downto 0);
-    m1_wb_we_i:  in std_logic;
-    m1_wb_cyc_i: in std_logic;
-    m1_wb_stb_i: in std_logic;
-    m1_wb_ack_o: out std_logic;
-    m1_wb_stall_o: out std_logic;
-
-    -- Slave signals
-
-    s0_wb_dat_i: in std_logic_vector(31 downto 0);
-    s0_wb_dat_o: out std_logic_vector(31 downto 0);
-    s0_wb_adr_o: out std_logic_vector(ADDRESS_HIGH downto ADDRESS_LOW);
-    s0_wb_sel_o: out std_logic_vector(3 downto 0);
-    s0_wb_cti_o: out std_logic_vector(2 downto 0);
-    s0_wb_we_o:  out std_logic;
-    s0_wb_cyc_o: out std_logic;
-    s0_wb_stb_o: out std_logic;
-    s0_wb_ack_i: in std_logic;
-    s0_wb_stall_i: in std_logic
-  );
-  end component;
-
-  component wb_singleport_ram is
-  generic (
-    bits: natural := 8
-  );
-  port (
-    wb_clk_i: in std_logic;
-	 	wb_rst_i: in std_logic;
-    wb_dat_o: out std_logic_vector(31 downto 0);
-    wb_dat_i: in std_logic_vector(31 downto 0);
-    wb_adr_i: in std_logic_vector(31 downto 0);
-    wb_we_i:  in std_logic;
-    wb_cyc_i: in std_logic;
-    wb_stb_i: in std_logic;
-    wb_ack_o: out std_logic;
-    wb_inta_o:out std_logic
-  );
-  end component;
-
-  component wb_master_np_to_slave_p is
-  generic (
-    ADDRESS_HIGH: integer := 31;
-    ADDRESS_LOW: integer := 0
-  );
-  port (
-    wb_clk_i: in std_logic;
-	 	wb_rst_i: in std_logic;
-
-    -- Master signals
-
-    m_wb_dat_o: out std_logic_vector(31 downto 0);
-    m_wb_dat_i: in std_logic_vector(31 downto 0);
-    m_wb_adr_i: in std_logic_vector(ADDRESS_HIGH downto ADDRESS_LOW);
-    m_wb_sel_i: in std_logic_vector(3 downto 0);
-    m_wb_cti_i: in std_logic_vector(2 downto 0);
-    m_wb_we_i:  in std_logic;
-    m_wb_cyc_i: in std_logic;
-    m_wb_stb_i: in std_logic;
-    m_wb_ack_o: out std_logic;
-
-    -- Slave signals
-
-    s_wb_dat_i: in std_logic_vector(31 downto 0);
-    s_wb_dat_o: out std_logic_vector(31 downto 0);
-    s_wb_adr_o: out std_logic_vector(ADDRESS_HIGH downto ADDRESS_LOW);
-    s_wb_sel_o: out std_logic_vector(3 downto 0);
-    s_wb_cti_o: out std_logic_vector(2 downto 0);
-    s_wb_we_o:  out std_logic;
-    s_wb_cyc_o: out std_logic;
-    s_wb_stb_o: out std_logic;
-    s_wb_ack_i: in std_logic;
-    s_wb_stall_i: in std_logic
-  );
-  end component;
-
-  component romram is
-  generic (
-    BITS: integer := 32
-  );
-  port (
-    ram_wb_clk_i:       in std_logic;
-    ram_wb_rst_i:       in std_logic;
-    ram_wb_ack_o:       out std_logic;
-    ram_wb_dat_i:       in std_logic_vector(31 downto 0);
-    ram_wb_dat_o:       out std_logic_vector(31 downto 0);
-    ram_wb_adr_i:       in std_logic_vector(BITS-1 downto 2);
-    ram_wb_sel_i:       in std_logic_vector(3 downto 0);
-    ram_wb_cyc_i:       in std_logic;
-    ram_wb_stb_i:       in std_logic;
-    ram_wb_we_i:        in std_logic;
-    ram_wb_stall_o:     out std_logic;
-
-    rom_wb_clk_i:       in std_logic;
-    rom_wb_rst_i:       in std_logic;
-    rom_wb_ack_o:       out std_logic;
-    rom_wb_dat_o:       out std_logic_vector(31 downto 0);
-    rom_wb_adr_i:       in std_logic_vector(BITS-1 downto 2);
-    rom_wb_cyc_i:       in std_logic;
-    rom_wb_stb_i:       in std_logic;
-    rom_wb_stall_o:     out std_logic
-  );
-  end component;
-
   component uart is
   generic (
     bits: integer := 11
@@ -185,62 +32,49 @@ architecture sim of tb is
 
   signal txd, rxd: std_logic;
 
+  signal wbi: wb_mosi_type;
+  signal wbo: wb_miso_type;
+  signal syscon: wb_syscon_type;
+  signal swbi: slot_wbi;
+  signal swbo: slot_wbo;
+  signal sids: slot_ids;
+
 begin
 
   rxd <= '1';
 
   w_clk <= not w_clk after period/2;
-  w_clk_2x <= not w_clk_2x after period/4;
 
+  syscon.clk<=w_clk;
+  syscon.rst<=w_rst;
 
   cpu: xtc_top_bram
   port map (
-    wb_syscon.clk   => w_clk,
-    wb_syscon.rst   => w_rst,
-
+    wb_syscon       => syscon,
     -- Master wishbone interface
-
-    iowbi.ack        => wb_ack,
-    iowbi.dat        => wb_read,
-    iowbi.int        => wb_int,
-    iowbi.tag        => wb_tag_i,
-    iowbi.stall      => '0',
-    iowbo.dat        => wb_write,
-    iowbo.adr        => wb_address,
-    iowbo.cyc        => wb_cyc,
-    iowbo.stb        => wb_stb,
-    iowbo.sel        => wb_sel,
-    iowbo.tag        => wb_tag_o,
-    iowbo.we         => wb_we
+    iowbi           => wbo,
+    iowbo           => wbi
   );
+
+  ioctrl: xtc_ioctrl
+    port map (
+      syscon      => syscon,
+      wbi         => wbi,
+      wbo         => wbo,
+      swbi        => swbi,
+      swbo        => swbo,
+      sids        => sids
+  );
+
 
   myuart: uart
     port map (
-      wb_clk_i    => w_clk,
-      wb_rst_i    => w_rst,
-      wb_dat_o    => wb_read,
-      wb_dat_i    => wb_write,
-      wb_adr_i    => wb_address(31 downto 2),
-      wb_we_i     => wb_we,
-      wb_cyc_i    => wb_cyc,
-      wb_stb_i    => wb_stb,
-      wb_ack_o    => wb_ack,
-      wb_inta_o   => open,
-  
-      tx          => txd,
-      rx          => rxd
+      syscon    => syscon,
+      wbi         => swbo(1),
+      wbo         => swbi(1),
+      tx        => txd,
+      rx        => rxd
   );
-
-  -- Simple tag generator
-  process(w_clk)
-  begin
-    if rising_edge(w_clk) then
-       if wb_cyc='1' and wb_stb='1' and wb_ack='0' then
-        wb_tag_i <= wb_tag_o;
-       end if;
-    end if;
-  end process;
-
 
   -- Reset procedure
   process
@@ -251,15 +85,6 @@ begin
     wait for period;
     w_rst<='0';
     wait;
-  end process;
-
-  -- Interrupt test
-  process
-  begin
-    wait for 310 ns;
-    wb_int <= '1';
-    wait for 50 ns;
-    wb_int <= '0';
   end process;
 
 end sim;
