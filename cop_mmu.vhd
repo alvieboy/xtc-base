e8f20d4f088680bc74a22df5656fce939c5d30aa
Alvaro <alvieboy@alvie.com>
2015-01-02 11:08:25 +0000

    Tracebuffers

--- cop_mmu.vhd
+++ cop_mmu.vhd
@@ -51,7 +51,7 @@ architecture behave of cop_mmu is
   --end record;
 
   subtype logtype is std_logic_vector(127 downto 0);
-  constant LOGSIZE: integer := 1;
+  constant LOGSIZE: integer := 16;
   type logbuffertype is array (0 to LOGSIZE-1) of logtype;
   shared variable logbuffer: logbuffertype;
   signal loghigh,logread: integer range 0 to LOGSIZE-1;
@@ -60,7 +60,7 @@ architecture behave of cop_mmu is
 
   -- memory debugging.
   subtype mlogtype is std_logic_vector(95 downto 0);
-  constant MLOGSIZE: integer := 1;
+  constant MLOGSIZE: integer := 32;
   type mlogbuffertype is array (0 to MLOGSIZE-1) of mlogtype;
   shared variable mlogbuffer: mlogbuffertype;
   signal mloghigh,mlogread: integer range 0 to MLOGSIZE-1;
@@ -103,10 +103,15 @@ begin
         ack<='0';
         icache_flush<='0';
         dcache_flush<='0';
-        mmatchen<='0';
+        mmatchen<='1';
+        mmatch<=x"001640E4";
         msetup<=false;
         enablelog<='0';
         proten<='0';
+        addr_q <= (others => 'X');
+        wrq    <= (others => 'X');
+        protw  <= (others => 'X');
+        co.data<= (others => 'X');
       else
         tlbw<='0';
         ack<='0';
@@ -144,7 +149,9 @@ begin
 
             when "0110" =>
               if ci.wr='1' then
-                enablelog<= ci.data(0);
+                if TRACER_ENABLED then
+                  enablelog<= ci.data(0);
+                end if;
                 proten<=ci.data(1);
               end if;
               co.data <= std_logic_vector(fflags);
@@ -157,43 +164,59 @@ begin
 
             -- TODO: move this to another COP
             when "1000" =>
-              if ci.wr='1' then
-                logread<=to_integer(unsigned(ci.data));
+              if TRACER_ENABLED then
+                if ci.wr='1' then
+                  logread<=to_integer(unsigned(ci.data));
+                end if;
+                co.data(15 downto 0) <= std_logic_vector(to_unsigned(loghigh,16));
+                co.data(31 downto 16) <= std_logic_vector(to_unsigned(LOGSIZE,16));
               end if;
-              co.data(15 downto 0) <= std_logic_vector(to_unsigned(loghigh,16));
-              co.data(31 downto 16) <= std_logic_vector(to_unsigned(LOGSIZE,16));
             when "1001" =>
-              co.data <= rlog(127 downto 96);
+              if TRACER_ENABLED then
+                co.data <= rlog(127 downto 96);
+              end if;
             when "1010" =>
-              co.data <= rlog(95 downto 64);
+              if TRACER_ENABLED then
+                co.data <= rlog(95 downto 64);
+              end if;
             when "1011" =>
-              co.data <= rlog(63 downto 32);
+              if TRACER_ENABLED then
+                co.data <= rlog(63 downto 32);
+              end if;
             when "1100" =>
-              co.data <= rlog(31 downto 0);
-
+              if TRACER_ENABLED then
+                co.data <= rlog(31 downto 0);
+              end if;
 
             when "0101" =>
-              if ci.wr='1' then
-                mlogread<=to_integer(unsigned(ci.data));
-              end if;
-              co.data(15 downto 0) <= std_logic_vector(to_unsigned(mloghigh,16));
-              co.data(31 downto 16) <= std_logic_vector(to_unsigned(MLOGSIZE,16));
-            when "1101" =>
-              co.data <= mrlog(95 downto 64);
-            when "1110" =>
-              if ci.wr='1' then
-                msetup<=true;
-                mmatch<=unsigned(ci.data);
-                if (ci.data/=x"FFFFFFFF") then
-                  mmatchen<='1';
-                else
-                  mmatchen<='0';
+              if TRACER_ENABLED then
+                if ci.wr='1' then
+                  mlogread<=to_integer(unsigned(ci.data));
                 end if;
+                co.data(15 downto 0) <= std_logic_vector(to_unsigned(mloghigh,16));
+                co.data(31 downto 16) <= std_logic_vector(to_unsigned(MLOGSIZE,16));
               end if;
-              co.data <= mrlog(63 downto 32);
+            when "1101" =>
+              if TRACER_ENABLED then
+                co.data <= mrlog(95 downto 64);
+              end if;
+            when "1110" =>
+              if TRACER_ENABLED then
+                if ci.wr='1' then
+                  msetup<=true;
+                  mmatch<=unsigned(ci.data);
+                  if (ci.data/=x"FFFFFFFF") then
+                    mmatchen<='1';
+                  else
+                    mmatchen<='0';
+                  end if;
+                end if;
+                co.data <= mrlog(63 downto 32);
+              end if;
             when "1111" =>
-              co.data <= mrlog(31 downto 0);
-
+              if TRACER_ENABLED then
+                co.data <= mrlog(31 downto 0);
+              end if;
             when others =>
           end case;
           ack<='1';
@@ -205,8 +228,7 @@ begin
 
   co.valid<=ack;
 
-  --logenabled <= '1';--dbgi.dbgen;
-  --mlogenabled <= '1';--dbgi.dbgen;
+  tracer: if TRACER_ENABLED generate
 
   -- TODO: trace
   process(clk)
@@ -221,12 +243,11 @@ begin
         end if;
         if logenabled='1' then
           if dbgi.valid and dbgi.executed then
-            loghigh<=loghigh+1;
-            -- synthesis translate_off
             if loghigh=LOGSIZE-1 then
               loghigh<=0;
+            else
+              loghigh<=loghigh+1;
             end if;
-            -- synthesis translate_on
             logbuffer(loghigh) := std_logic_vector(dbgi.pc) &
               std_logic_vector(dbgi.lhs) &
               std_logic_vector(dbgi.rhs) &
@@ -251,7 +272,7 @@ begin
   begin
     if rising_edge(clk) then
       if rst='1' then
-        mlogenabled<='0';
+        mlogenabled<='1';
         mloghigh<=0;
       else
         if msetup then
@@ -261,11 +282,11 @@ begin
           mlogenabled<='0';
         end if;
         if mlogenabled='1' then
-          if mdbgi.strobe='1' and (mmatchen='0' or mdbgi.data=mmatch) and mdbgi.write='1' then
-            mloghigh<=mloghigh+1;
-            -- synthesis translate_off
+          if mdbgi.strobe='1' and (mmatchen='0' or mdbgi.address=mmatch) and mdbgi.write='1' then
             if mloghigh=MLOGSIZE-1 then
               mloghigh<=0;
+            else
+              mloghigh<=mloghigh+1;
             end if;
             -- synthesis translate_on
             mlogbuffer(mloghigh) :=
@@ -284,5 +305,6 @@ begin
     end if;
   end process;
 
+  end generate;
 
 end behave;
